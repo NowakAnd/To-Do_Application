560d2b7423dec714dedb92aa50b51b79eb44f911
Andrzej Nowak <andrzejnowak614@gmail.com>
2024-03-22 22:50:03 +0100

    Created Add and list functionalities

--- tests/test_rptodo.py
+++ tests/test_rptodo.py
@@ -1,9 +1,12 @@
 # unit tests
 # tests/test_rptodo.py
 
+import json
+
+import pytest
 from typer.testing import CliRunner
 
-from rptodo import __app_name__, __version__, cli
+from rptodo import __app_name__, __version__, cli, DB_READ_ERROR, SUCCESS, rptodo
 
 runner = CliRunner()
 
@@ -25,3 +28,54 @@ def test_version_1_negative():
     assert result.exit_code == 2
     assert "Error: No such option:" in result.stdout
 
+
+@pytest.fixture
+def mock_json_file(tmp_path):
+    todo = [{"Description": "Get some milk.", "Priority": 2, "Done": False}]
+    db_file = tmp_path / "todo.json"
+    with db_file.open('w') as db:
+        json.dump(todo, db, indent=4)
+    return db_file
+
+
+test_data1 = {
+    "description": ["Clean", "the", "house"],
+    "priority": 1,
+    "todo": {
+        "Description": "Clean the house.",
+        "Priority": 1,
+        "Done": False,
+    },
+}
+
+test_data2 = {
+    "description": ["Wash the car"],
+    "priority": 2,
+    "todo": {
+        "Description": "Wash the car.",
+        "Priority": 2,
+        "Done": False,
+    },
+}
+
+
+@pytest.mark.parametrize(
+    "description, priority, expected",
+    [
+        pytest.param(
+            test_data1["description"],
+            test_data1["priority"],
+            (test_data1["todo"], SUCCESS),
+        ),
+        pytest.param(
+            test_data2["description"],
+            test_data2["priority"],
+            (test_data2["todo"], SUCCESS),
+        ),
+    ],
+)
+def test_add(mock_json_file, description, priority, expected):
+    todoer = rptodo.Todoer(mock_json_file)
+    assert todoer.add(description, priority) == expected
+    read = todoer._db_handler.read_todos()
+    assert len(read.todo_list) == 2
